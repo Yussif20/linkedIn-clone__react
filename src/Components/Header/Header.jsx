249989f0d7b2e0e1f70ee83f0beb53caefdcc4ce
Yousef <yousefayman2000500@gmail.com>
2023-02-12 22:10:02 +0200

    adding the styles to the left header side

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import "./Header.css";
 import Logo from "../../Assets/Images/linkedin.png";
 import SearchIcon from "@mui/icons-material/Search";
 function Header() {
   return (
     <div className="header">
-      <h1>This is the header</h1>
       <div className="header__left">
         <img src={Logo} alt="linkedin logo" />
         <div className="header__search">
@@ -12,6 +11,7 @@ function Header() {
           <input type="text" />
         </div>
       </div>
+
       <div className="header__right"></div>
     </div>
   );
