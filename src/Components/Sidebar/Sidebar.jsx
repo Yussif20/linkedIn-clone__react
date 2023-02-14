b61d6cef4f4807f01c62d960926f8eba42b37040
Yousef <yousefayman2000500@gmail.com>
2023-02-14 21:09:47 +0200

    finishing the sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,26 +4,40 @@ import AvatarImage from "../../Assets/Images/avatar.jpg";
 import BackgroundImage from "../../Assets/Images/background.jpeg";
 
 function Sidebar() {
+  const recentItem = (topic) => {
+    return (
+      <div className="sidebar__recentItem">
+        <span className="sidebar__hash">#</span>
+        <p>{topic}</p>
+      </div>
+    );
+  };
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <img src={BackgroundImage} alt="avatar background" />
         <Avatar className="sidebar__avatar" src={AvatarImage} />
         <h2>Yousef Ayman</h2>
-        <h2>yousefayman2000500@gmail.com</h2>
+        <h4>yousefayman2000500@gmail.com</h4>
       </div>
       <div className="sidebar__stats">
         <div className="sidebar__stat">
-          <p>Who viewed your profile</p>
+          <p>Profile Views</p>
           <p className="sidebar__statNumber">2,789</p>
         </div>
         <div className="sidebar__stat">
           <p>Views on post</p>
           <p className="sidebar__statNumber">2,119</p>
         </div>
-        <div className="sidebar__bottom">
-          <p></p>
-        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("React")}
+        {recentItem("Javascript")}
+        {recentItem("Css")}
+        {recentItem("Design")}
+        {recentItem("Developer")}
+        {recentItem("Html")}
       </div>
     </div>
   );
