6905f3d64a4291fab590cc52d89c4068c115f366
Yousef <yousefayman2000500@gmail.com>
2023-02-15 04:40:38 +0200

    building the input section

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,6 +1,10 @@
 import "./Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
-
+import InputOption from "../InputOption/InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import ArticleIcon from "@mui/icons-material/Article";
 function Feed() {
   return (
     <div className="feed">
@@ -11,6 +15,16 @@ function Feed() {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOption">
+          <InputOption Icon={ImageIcon} color="#70B5F9" title="Photo" />
+          <InputOption Icon={SubscriptionsIcon} color="#E7A33E" title="Video" />
+          <InputOption Icon={EventNoteIcon} color="#C0CBCD" title="Event" />
+          <InputOption
+            Icon={ArticleIcon}
+            color="#7fc15e"
+            title="Write Article"
+          />
         </div>
       </div>
     </div>
